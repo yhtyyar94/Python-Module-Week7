c66e4111ee2986f187a093462fd114b4456a05bd
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-10-19 13:17:27 +0200

    delete redirections file

--- redirections.py
+++
@@ -1,9 +0,0 @@
-from admin_menu import AdminUI
-from mentor_menu import MentorUI
-from application_menu import ApplicationsUI
-
-buttons = {
-    "admin_menu": AdminUI(),
-    "mentor_menu": MentorUI(),
-    "applications_menu": ApplicationsUI(),
-}
