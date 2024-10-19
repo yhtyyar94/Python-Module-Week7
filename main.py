f371684c07f8c00326f6d03c0730f446ea7241af
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-10-19 12:28:36 +0200

    merve abla's page merged

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     from admin_menu import Ui_MainWindow as AdminUI
     from application_menu import Ui_MainWindow as ApplicationsUI
     from mentor_menu import Ui_MainWindow as MentorUI
+    from interviews_menu import Ui_MainWindow as InterviewsUI
 
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
@@ -17,12 +18,14 @@ if __name__ == "__main__":
     admin_menu = AdminUI()
     applications_menu = ApplicationsUI()
     mentor_menu = MentorUI()
+    interviews_menu = InterviewsUI()
 
     def admin_setup():
         admin_menu.setupUi(MainWindow)
         admin_menu.Basvurular.clicked.connect(lambda: applications_setup())
 
         admin_menu.Mentor.clicked.connect(lambda: mentor_setup())
+        admin_menu.Basvurular_2.clicked.connect(lambda: interviews_menu_setup())
         admin_menu.pushButton.clicked.connect(MainWindow.close)
 
     def applications_setup():
@@ -34,6 +37,11 @@ if __name__ == "__main__":
         mentor_menu.setupUi(MainWindow)
         mentor_menu.pushButton_5.clicked.connect(admin_setup)
         mentor_menu.pushButton_4.clicked.connect(MainWindow.close)
+
+    def interviews_menu_setup():
+        interviews_menu.setupUi(MainWindow)
+        interviews_menu.pushButton_tercih.clicked.connect(admin_setup)
+        interviews_menu.pushButton_exit.clicked.connect(MainWindow.close)
 
     ui.pushButton.clicked.connect(admin_setup)
 
