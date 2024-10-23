848b73bda0a996ed91c989773452c431d2831e87
Yakup Atas <yakup.atas.global@gmail.com>
2024-10-23 16:00:35 +0200

    test

--- backend/auth.py
+++ backend/auth.py
@@ -19,7 +19,7 @@ def authenticate():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "./backend/credentials.json", SCOPES
+                "credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
---
+++ test.py
@@ -0,0 +1,13 @@
+from backend.list_files import list_drive_files
+from backend.download_file import download_file
+
+
+file_lists = list_drive_files()
+for file in file_lists:
+    print(f"file : {file["name"]} fileID : {file["id"]}")
+# Kullanicilar.xlsx fileID : 1jQmJFlS5y9ydZJFnL_Dt4lJBxBZU9fmX
+# Mentor.xlsx fileID: 1VIGenwWW4gBECB5lFwkV2D2k2xTDaL6c
+# Mulakatlar.xlsx fileID: 1jg6LcEbdGf-kSijN-YEum6JrasgNaoZi
+# Basvurular.xlsx fileID : 1hpTJXh-NLbOyjHV1aTn6-XqzjYRRYdej
+
+# download_file("1VIGenwWW4gBECB5lFwkV2D2k2xTDaL6c")
