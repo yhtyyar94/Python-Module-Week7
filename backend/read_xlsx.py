00119ac79c32e54d4d4da46691e684428e7a443e
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-10-24 09:08:04 +0200

    read xlsx is merged

---
+++ backend/read_xlsx.py
@@ -0,0 +1,19 @@
+from openpyxl import load_workbook
+
+
+def read_xlsx(file_name):
+    # Load the .xlsx file
+    workbook = load_workbook(filename=file_name)
+
+    # Select the active worksheet
+    sheet = workbook.active
+
+    rows = []
+
+    # Loop through all rows and columns and filter empty rows
+    for row in sheet.iter_rows(min_row=1, values_only=True):
+        if all(cell is None for cell in row):
+            continue
+        rows.append(row)
+    rows = [list(row) for row in rows]
+    return rows
